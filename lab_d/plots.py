88ba23a5f0c7a7135e17e70e0a6edf274b53ddce
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-06-18 21:39:55 +0200

    refactoring

--- lab_d/plots.py
+++ lab_d/plots.py
@@ -1,76 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv 
 
 ds = [ 16, 32, 64, 128, 256 ]
 ps = [ 0.01, 0.02, 0.05, 0.1, 0.2, 0.5 ]
 
-def plot_linear():
-    cost = np.zeros((len(ds),len(ps)))
-    copies = np.zeros((len(ds),len(ps)))
+def read_data(parameters):
+    data = { parameter: np.zeros((len(ds),len(ps))) for parameter in parameters }
 
-    for i in range(len(ds)):
-        for j in range(len(ps)):             
-            filename = f"data/d_{ds[i]}_p_{ps[j]}_allocation.txt"
-            file = open(filename, 'r')
-            line = file.readline().split(",")
-            cost[i][j]   = int(float(line[0]))
-            copies[i][j] = float(line[1])
-  
+    with open('data/allocation_experiment.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            d = int(row['d'])
+            p = float(row['p'])
+            i = ds.index(d)
+            j = ps.index(p)
+            for parameter in parameters:
+                data[parameter][i][j] = float(row[parameter])
+
+    return data
+
+def plot_linear(parameter, data):
     plt.figure().set_figwidth(10)
-    for i,d in enumerate(ds):
-        plt.plot(ps, cost[i][:], label=d, marker='o')
+    for i, d in enumerate(ds):
+        plt.plot(ps, data[i][:], label=f'd={d}', marker='o')
     plt.xlabel('p')
-    plt.ylabel('avg_cost')
+    plt.ylabel(parameter)
     plt.legend()
-    plt.savefig("data/cost_ds.png", dpi=300)
+    plt.savefig(f'data/{parameter}_linear.png')
     plt.close()
-
-    plt.figure().set_figwidth(10)
-    for i,d in enumerate(ds):
-        plt.plot(ps, copies[i][:], label=d, marker='o')
-    plt.xlabel('p')
-    plt.ylabel('max_copies')
-    plt.legend()
-    plt.savefig("data/copies_ds.png", dpi=300)
-    plt.close()
-
-def read_floats(d, p):
-    filename = f"data/d_{d}_p_{p}_allocation.txt"
-    file = open(filename, 'r')
-    line = file.readline()
-    return [ float (x) for x in line.split(",") ]
-
-def plot_heatmap(parameter, idx):
-    data = [ [ (read_floats(d, p))[idx] for d in ds ] for p in ps ]
-    data = [ [int(value) for value in row] for row in data ] if parameter == "avg_cost" else data
-    data = np.array(data)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(data)
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(len(ds)))
-    ax.set_yticks(np.arange(len(ps)))
-    # ... and label them with the respective list entries
-    ax.set_xticklabels(ds)
-    ax.set_yticklabels(ps)
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-            rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(ps)):
-        for j in range(len(ds)):
-            text = ax.text(j, i, data[i, j],
-                        ha="center", va="center", color="w")
-
-    ax.set_title(parameter)
-    fig.tight_layout()
-    #plt.show()
-    plt.savefig(f'data/{parameter}_heatmap.png')
 
 if __name__ == "__main__":
-    for (parameter, idx) in [ ("avg_cost", 0), ("avg_copies", 1) ]:
-        plot_heatmap(parameter, idx)
-    plot_linear()
+    parameters = ["avg_cost", "avg_max_copies"]
+    data = read_data(parameters)
+    for parameter in parameters:
+        plot_linear(parameter, data[parameter])
--- lab_d/src/main.rs
+++ lab_d/src/main.rs
@@ -5,18 +5,18 @@ use page_allocation::*;
 use std::fs::File;
 use std::io::Write;
 
-const NUM_OF_TESTS_U : usize = 1000;
+const NUM_OF_TESTS_U : usize = 10_000;
 const NUM_OF_TESTS_F : f64 = NUM_OF_TESTS_U as f64;
 
 fn experiment() {
     let ds = [16, 32, 64, 128, 256];
     let ps = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5];
 
+    let mut file = File::create("data/allocation_experiment.csv").unwrap();
+    writeln!(file, "p,d,avg_cost,avg_max_copies").unwrap();
+
     for d in ds {
         for p in ps {
-            let filename = format!("data/d_{}_p_{}_allocation.txt", d, p);
-            let mut file = File::create(filename).unwrap();
-
             let mut avg_cost = 0.0;
             let mut avg_max_copies = 0.0;
 
@@ -29,8 +29,7 @@ fn experiment() {
             avg_cost /= NUM_OF_TESTS_F;
             avg_max_copies /= NUM_OF_TESTS_F;
 
-            let line = format!("{}, {}", avg_cost, avg_max_copies);
-            file.write_all(line.as_bytes()).unwrap();
+            writeln!(file, "{},{},{},{}", p, d, avg_cost, avg_max_copies).unwrap();
         }
     }
 }
